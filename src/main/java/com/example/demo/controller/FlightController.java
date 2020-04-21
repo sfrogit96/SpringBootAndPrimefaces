5bf4340c12233359b6a940bd453008b6e8e98743
Thien <sfrogit96@outlook.com>
2020-04-21 17:59:09 +0700

    first commit

---
+++ src/main/java/com/example/demo/controller/FlightController.java
@@ -0,0 +1,59 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.model.Flight;
+import com.example.demo.repository.FlightRepository;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class FlightController implements Serializable {
+
+    private Flight flight = new Flight();
+
+    private List<Flight> flights = new ArrayList<>();
+
+    @Autowired
+    private FlightRepository flightRepository;
+
+    public void fetchAll() {
+        flights = flightRepository.findAll();
+    }
+
+    public void save() {
+        flightRepository.save(flight);
+        flight = new Flight();
+        flights = flightRepository.findAll();
+    }
+
+    public void edit(Flight flight) {
+        this.flight = flight;
+    }
+
+    public void refresh() {
+        flight = new Flight();
+    }
+
+    public List<Flight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(List<Flight> flights) {
+        this.flights = flights;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/Flight.java
@@ -0,0 +1,68 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+public class Flight {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String airline;
+    private String fromAirport;
+    private String toAirport;
+
+    private Date scheduledDateTime;
+    private Date estimatedDateTime;
+    private Date actualDateTime;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getAirline() {
+		return airline;
+	}
+	public void setAirline(String airline) {
+		this.airline = airline;
+	}
+	public String getFromAirport() {
+		return fromAirport;
+	}
+	public void setFromAirport(String fromAirport) {
+		this.fromAirport = fromAirport;
+	}
+	public String getToAirport() {
+		return toAirport;
+	}
+	public void setToAirport(String toAirport) {
+		this.toAirport = toAirport;
+	}
+	public Date getScheduledDateTime() {
+		return scheduledDateTime;
+	}
+	public void setScheduledDateTime(Date scheduledDateTime) {
+		this.scheduledDateTime = scheduledDateTime;
+	}
+	public Date getEstimatedDateTime() {
+		return estimatedDateTime;
+	}
+	public void setEstimatedDateTime(Date estimatedDateTime) {
+		this.estimatedDateTime = estimatedDateTime;
+	}
+	public Date getActualDateTime() {
+		return actualDateTime;
+	}
+	public void setActualDateTime(Date actualDateTime) {
+		this.actualDateTime = actualDateTime;
+	}
+
+}
